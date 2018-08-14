4f4a2fbd6e6a2d2423d05add89441cb03983ac2b
yuda <akuntestinguntukseto@gmail.com>
2018-08-14 11:06:29 +0700

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+import {Platform, NativeModules, Dimensions} from 'react-native';
+const {LinkingManager, IntentAndroid} = NativeModules;
+
+export const Linking = isIOS ? LinkingManager : IntentAndroid;
+
+const {width, height} = Dimensions.get('window');
+
+let isGTELOLLIPOP = true
+if (!isIOS) {
+  isGTELOLLIPOP = Platform.Version > 22
+}
+
+export {
+	width,
+	height,
+	isGTELOLLIPOP 
+}
